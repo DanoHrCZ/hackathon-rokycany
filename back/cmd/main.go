88a77976291628a484072e983180f116700053c8
Glebegor <glebegor2220@gmail.com>
2024-01-30 08:42:48 +0100

    Created basic architecture of project.

---
+++ back/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// // Initialize of the project
+	// app := bootstrap.App()
+	// env := app.Env
+	// db := app.Database
+
+	// // Setup of git framework
+	// gin := gin.Default()
+	// gin.Use(bootstrap.CORS())
+
+	// timeout := 10 * time.Second
+	// route.NewRoute(env, timeout, db, gin)
+	// gin.Run(":" + env.SERVERport)
+}
